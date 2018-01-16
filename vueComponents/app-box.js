e341add84c1f9aafe7a6bce0018f7f586cb455a8
Richard Wong <ricwtk@gmail.com>
2018-01-16 17:44:04 +0800

    add tags to app

--- vueComponents/app-box.js
+++ vueComponents/app-box.js
@@ -12,6 +12,10 @@ Vue.component("app-box", {
         <div class="app-title" v-html="app.name"></div>
         <div class="app-action fa fa-angle-right"  @click="openurl()"></div>
         <div class="app-content">{{ app.description }}</div>
+        <div class="app-tags" v-if="app.tags">
+          tags:
+          <span class="app-tag" v-for="tag in app.tags" v-html="tag"></span>
+        </div>
       </div>
     </div>
   `
